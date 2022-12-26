6f6fe43c25543ecea226cec88c62cbd34fce19f1
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2022-12-26 19:46:35 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const { PORT } = require('./config/serverConfig');
 //const CityRepository = require('./repository/city-repository');
 const ApiRoutes = require('./routes/index');
 
-
+const db = require('./models/index');
+// const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
      //create a express object
@@ -19,7 +20,14 @@ const setupAndStartServer = async () => {
       app.use('/api', ApiRoutes);
 
      app.listen(PORT, async () => {
+          
         console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB){
+          db.sequelize.sync({alter: true});
+        }
+     //    await Airplane.create({
+     //      modelNumber : 'Bombardier CRJ'
+     //    })
 
 
 
